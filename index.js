c3f42b4679c7eaf6d113c49922353c71495c4281
Yong Park <pyongho0306@gmail.com>
2024-05-18 23:38:37 -0700

    Added group code

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const admin = require("firebase-admin");
 
 // Parse the JSON string from the environment variable
 const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+// const serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -17,14 +18,15 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
-app.post("/add-schedule", async (req, res) => {
-  const { employeeName, schedule } = req.body;
+pp.post("/add-schedule", async (req, res) => {
+  const { employeeName, schedule, group } = req.body;
   try {
     await db.collection("schedules").add({
       employeeName,
       schedule,
+      group,
     });
     res.status(200).send("Schedule added");
   } catch (error) {
@@ -33,8 +35,12 @@ app.post("/add-schedule", async (req, res) => {
 });
 
 app.get("/get-schedules", async (req, res) => {
+  const group = req.query.group;
   try {
-    const schedulesSnapshot = await db.collection("schedules").get();
+    const schedulesSnapshot = await db
+      .collection("schedules")
+      .where("group", "==", group)
+      .get();
     const schedules = [];
     schedulesSnapshot.forEach((doc) => {
       schedules.push({ id: doc.id, ...doc.data() });
@@ -45,8 +51,23 @@ app.get("/get-schedules", async (req, res) => {
   }
 });
 
+app.get("/get-employees", async (req, res) => {
+  const group = req.query.group;
+  try {
+    const employeesSnapshot = await db
+      .collection("users")
+      .where("group", "==", group)
+      .get();
+    const employees = [];
+    employeesSnapshot.forEach((doc) => {
+      employees.push({ uid: doc.id, ...doc.data() });
+    });
+    res.status(200).send(employees);
+  } catch (error) {
+    res.status(500).send("Error getting employees");
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-module.exports = app; // Ensure to export the app for Vercel
